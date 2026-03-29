118777769196134ee1be22256e01964533832cb6
Atharva Joshi <jatharva1701@gmail.com>
2026-03-29 15:51:35 +0530

    flex-grow to grow

--- client/src/components/AdminLayout.jsx
+++ client/src/components/AdminLayout.jsx
@@ -66,7 +66,7 @@ export default function AdminLayout() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-grow ml-64 p-8">
+      <div className="grow ml-64 p-8">
         <Outlet />
       </div>
     </div>
